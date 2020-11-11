5d44823e895119316550adc7cc9169d22ae867a1
augustofb2 <augustofb2@gmail.com>
2020-11-11 16:26:20 -0300

    projeto final concluido

---
+++ ProjetoSpringBoot/springboot/src/main/java/com/cursospring/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.cursospring.springboot.thymeleafdemo.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cursospring.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	
+	// add a method to sort by last name
+	public List<Employee> findAllByOrderByLastNameAsc();
+	
+}
